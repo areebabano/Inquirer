fcb3e5d3b0100cf1d472e5e0c7f8d427eee56909
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2024-06-24 23:06:44 +0500

    Inquirer in typescript

---
+++ index.js
@@ -0,0 +1,21 @@
+// Inquirer
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([{
+        name: "name",
+        type: "input",
+        message: "Enter your name: "
+    },
+    {
+        name: "age",
+        type: "number",
+        message: "Enter your age: "
+    },
+    {
+        name: "course",
+        type: "list",
+        message: "Choose your course",
+        choices: ["Python", "JS", "TS", "Html", "Css"]
+    }]);
+console.log(answer.name);
+console.log(answer.age);
+console.log(answer.course);
